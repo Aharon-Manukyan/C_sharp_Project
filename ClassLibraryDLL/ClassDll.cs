1fbdb28fcef254525a4fe1ef3dfbbce0f1d9f0f6
aharon <03manukyan03@gmail.com>
2021-03-13 15:23:12 +0300

    Добавьте файлы проекта.

---
+++ ClassLibraryDLL/ClassDll.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassLibraryDLL
+{
+    public class ClassDll
+    {
+        private Dictionary<string,int> ChangeText(string text)
+        {
+
+            Dictionary<string, int> dict = new Dictionary<string, int> { };
+            char[] letters = new char[25];
+            string word = " ";
+
+            foreach (char letter in text)
+            {
+                if (char.IsLetter(letter))
+                {
+                    word += char.ToString(char.ToLower(letter));
+                }
+                else if (char.IsDigit(letter) || letter == '_')
+                {
+                    continue;
+                }
+                else
+                {
+                    if (word != " ")
+                    {
+                        if (dict.ContainsKey(word))
+                        {
+                            dict[word] += 1;
+                        }
+                        else
+                        {
+                            dict[word] = 1;
+                        }
+                        letters = new char[25];
+                        word = " ";
+                    }
+                }
+            }
+            dict = dict.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            return dict;
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using ClassLibraryDLL;
+using System.Reflection;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {    
+        static void Main(string[] args)
+        {
+            try
+            {
+                string text = File.ReadAllText(@"C:\\Users\\User\\Desktop\\C#\\input.txt", Encoding.Default);
+                string docPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                
+                ClassDll classDll = new ClassDll();           
+                MethodInfo meth = classDll.GetType().GetMethod("ChangeText", BindingFlags.NonPublic | BindingFlags.Instance);
+                
+                var returnValue =  meth.Invoke(classDll, new object[] {text});
+                var dict = (Dictionary<string,int>)returnValue;
+
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "C#\\output.txt")))
+                {
+                    foreach (var d in dict)
+                    {
+                        if ((d.Key != ""))
+                        {
+                            outputFile.WriteLine(d.Key + "  " + d.Value);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
